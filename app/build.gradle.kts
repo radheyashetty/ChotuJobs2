dc1c7568af3e5b3017e1c0a318aae564cbd0c93f
radhe <radheyashetty167@gmail.com>
2025-11-04 05:08:35 +0530

    small commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,8 @@ dependencies {
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
     // Testing
     testImplementation(libs.junit)
